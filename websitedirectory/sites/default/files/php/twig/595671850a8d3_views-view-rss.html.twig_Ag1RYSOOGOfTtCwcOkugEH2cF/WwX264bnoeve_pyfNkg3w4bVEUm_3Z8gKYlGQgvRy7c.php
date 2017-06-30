80eff4579b2dc55775532385b77f33d4f51f452d
Kushal Hada <kushaldeveloper@gmail.com>
2017-06-30 17:29:03 -0400

    add everything

---
+++ websitedirectory/sites/default/files/php/twig/595671850a8d3_views-view-rss.html.twig_Ag1RYSOOGOfTtCwcOkugEH2cF/WwX264bnoeve_pyfNkg3w4bVEUm_3Z8gKYlGQgvRy7c.php
@@ -0,0 +1,107 @@
+<?php
+
+/* core/modules/views/templates/views-view-rss.html.twig */
+class __TwigTemplate_e12333893656d11068bbeb879e552b7f268808696e4abb876d460fb96874a652 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 20
+        echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>
+<rss version=\"2.0\" xml:base=\"";
+        // line 21
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["link"] ?? null), "html", null, true));
+        echo "\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["namespaces"] ?? null), "html", null, true));
+        echo ">
+  <channel>
+    <title>";
+        // line 23
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["title"] ?? null), "html", null, true));
+        echo "</title>
+    <link>";
+        // line 24
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["link"] ?? null), "html", null, true));
+        echo "</link>
+    <description>";
+        // line 25
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["description"] ?? null), "html", null, true));
+        echo "</description>
+    <language>";
+        // line 26
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["langcode"] ?? null), "html", null, true));
+        echo "</language>
+    ";
+        // line 27
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["channel_elements"] ?? null), "html", null, true));
+        echo "
+    ";
+        // line 28
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["items"] ?? null), "html", null, true));
+        echo "
+  </channel>
+</rss>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/views/templates/views-view-rss.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  73 => 28,  69 => 27,  65 => 26,  61 => 25,  57 => 24,  53 => 23,  46 => 21,  43 => 20,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "core/modules/views/templates/views-view-rss.html.twig", "C:\\xampp\\htdocs\\testing\\websitedirectory\\core\\modules\\views\\templates\\views-view-rss.html.twig");
+    }
+}
